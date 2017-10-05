73a25f85dbea2cbaf6d1d3135d22f6dd570696e3
SFrancesco <31933430+SFrancesco@users.noreply.github.com>
2017-10-05 14:40:37 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file consists in two functions.
+## The pourpose of these functions is to give an example of how we can save time and CPU's resources.
+## Discerning when it comes to computing an instruction and when it is the case to avoid computing, because it would return 
+## a value previously obtained, we should be able to minimize CPU and RAM consuming.
 
-## Write a short comment describing this function
 
+## makeCacheMatrix() returns a list of functions that will handle the contents of a matrix, passed as an argument to it by default.
+## Recalling these functions, which will handle global variables, we will be able to keep track of the changes made to the matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,
+             setInv = setinv, getInv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve(<matrix>, ...) is responsible for determining whether or not the data in the matrix has changed.
+## In the positive case, it will processes the inverse of the matrix passed as an argument to makeCacheMatrix,
+## stores it in a global variable, and then returns it.
+## In the negative case, it will returns the previous result of the inverse matrix, stored in that global variable.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInv()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setInv(inv)
+        inv
 }
